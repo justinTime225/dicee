6607516aadebcca20a805e14e7890f9740efebc1
Justin Nguyen <justinminh225@gmail.com>
2018-05-22 10:51:17 -0700

    test changes

--- testscript.sh
+++ testscript.sh
@@ -1,15 +1,6 @@
 
 #!/usr/bin/env bash
 
-if [[ "$BUDDYBUILD_BRANCH" =~ "qa" ]]; then
-cd $BUDDYBUILD_WORKSPACE
-cd Battlecruiser/Battlecruiser/Global
-sed -i "" "s/appProduction = true/appProduction = false/g" Environment.swift
-sed -i "" "s/analyticsProduction = true/analyticsProduction = false/g" Environment.swift
-sed -i "" "s/apiEnvironment = 0/apiEnvironment = 1/g" Environment.swift
-cat Environment.swift
-fi
-
 if [[ "$BUDDYBUILD_BRANCH" =~ "staging" ]]; then
 cd $BUDDYBUILD_WORKSPACE
 cd Battlecruiser/Battlecruiser/Global
