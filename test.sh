77e96dd513cffae377ec549892db5188a55398c5
Justin Nguyen <justinminh225@gmail.com>
2018-05-22 12:50:10 -0700

    file

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 
 #!/usr/bin/env bash
+echo "Starting prebuild script!"
+echo "Working directory: $PWD"
+
+echo "Patching sample.txt...."
 sed -i "" "s/apiEnvironment = 5/apiEnvironment = 0/g" sample.txt
+
+echo "Sample.txt contents:"
+
 cat sample.txt
 git add .
 git commit -m 'Production Build'
