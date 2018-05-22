f644f6a7f044bda005f76d512b9ede42fb847307
Justin Nguyen <justinminh225@gmail.com>
2018-05-22 13:22:44 -0700

    new test sh

--- test.sh
+++ test.sh
@@ -8,7 +8,9 @@ sed -i "" "s/apiEnvironment = 5/apiEnvironment = 0/g" sample.txt
 echo "Sample.txt contents:"
 
 cat sample.txt
+git config --global user.name "Justin"
+git config --global user.email justinminh225@gmail.com
 git add .
 git commit -m 'Production Build'
-git push 2>&1
+git push
 
